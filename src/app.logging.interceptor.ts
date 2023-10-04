6455907b8dbdc45bd776622a47c8483d6e900d54
goldbearman <sabirovfarit@gmail.com>
2023-10-04 17:56:06 +0300

    rxjs

---
+++ src/app.logging.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+    CallHandler,
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    InternalServerErrorException
+} from '@nestjs/common';
+import {Reflector} from '@nestjs/core';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+    constructor(private readonly reflector: Reflector) {}
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        console.log({
+           handler:context.getClass().name,
+           className: context.getHandler().name
+        })
+        return next.handle();
+    }
+}
--- src/book/books.controller.ts
+++ src/book/books.controller.ts
@@ -1,10 +1,12 @@
-import {Controller, Post, Body, Get, Put, Param, Delete,} from '@nestjs/common';
+import {Controller, Post, Body, Get, Put, Param, Delete,UseInterceptors} from '@nestjs/common';
 import {BooksService} from './books.service';
 import {CreateBookDto} from './interfaces/book.interface';
 import {BookDocument} from './schemas/book.schema';
 import {IParamId} from "./interfaces/param.id";
 import {UpdateBook} from "./interfaces/update.book";
+import {LoggingInterceptor} from "../app.logging.interceptor";
 
+@UseInterceptors(LoggingInterceptor)
 @Controller('books')
 export class BooksController {
     constructor(private readonly bookService: BooksService) {
